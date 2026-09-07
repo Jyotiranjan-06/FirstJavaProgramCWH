05e15b8c9c9739107090236042568f298f855807
Jyoti <jyotiranjansatapathy67@gmail.com>
2026-09-07 13:11:53 +0530

    Added new Java program

---
+++ src/conditionals_09.java
@@ -0,0 +1,11 @@
+public class conditionals_09 {
+    public static void main(String[] args) {
+        int age = 12;
+        if (age >= 18) {
+            System.out.println("Yes you can drive!");
+        }
+        else {
+            System.out.println("No boy you cannot drivr yet!");
+        }
+    }
+}
